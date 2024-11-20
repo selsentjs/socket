0d728e1ec3f13eb25bd92707ac6dcdc3aa0aee50
selvisentest2006 <selvisentest2006@gmail.com>
2024-11-20 18:54:52 +0530

    Stop tracking .env file

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,14 +1,18 @@
 const User = require("../Model/User");
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const generateTokenAndSetCookie = require("../utils/getToken");
 
 // signup / register user
 const Register = async (req, res) => {
-  const { fullName, username, password,confirmPassword, gender } = req.body;
-// check password and confirmPassword
+  const { fullName, username, password, confirmPassword, gender } = req.body;
+  // check password and confirmPassword
 
-if(password !== confirmPassword){
-   return res.status(400).json({msg:"password and confirm password should match"})
-}
+  if (password !== confirmPassword) {
+    return res
+      .status(400)
+      .json({ msg: "password and confirm password should match" });
+  }
   // check username
   const usernameAlreadyExists = await User.findOne({ username });
   if (usernameAlreadyExists) {
@@ -17,39 +21,49 @@ if(password !== confirmPassword){
 
   // hash password
   const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password,salt)
+  const hashedPassword = await bcrypt.hash(password, salt);
   // https://avatar-placeholder.iran.liara.run/#document
-  
-  const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
-  const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+  const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+  const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
   try {
-     // Create a new user
-     const newUser = await User.create({
-        fullName,
-        username,
-        password: hashedPassword,
-        gender,
-        profilePic: gender === "male" ? boyProfilePic: girlProfilePic,
-      });
-  
-      // Save the new user
-      const user = await newUser.save();
-  
-      // Respond with success
-      res.status(201).json({ msg: "User registered", user });
+    // Create a new user
+    const newUser = await User.create({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    // before save generate token
+    generateTokenAndSetCookie(newUser._id, res);
+    // Save the new user
+    const user = await newUser.save();
+
+    // Respond with success
+    res.status(201).json({ msg: "User registered", user });
   } catch (err) {
     res.status(500).json({ msg: "Server error", error: err.message });
   }
 };
- // =====================================================================
+// =====================================================================
 // login
 const Login = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(404).json({ msg: "Please provide email and password" });
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(404).json({ msg: "Please provide username and password" });
   }
   try {
-    res.status(200).json({ msg: "user logged in" });
+    const user = await User.findOne({username})
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "")
+    
+    if(!user || !isPasswordCorrect) {
+        return res.status(400).json({msg:"Invalid username or password"})
+    }
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({ msg: "user logged in", user });
   } catch (err) {
     res.status(500).json({ msg: "Server error", error: err.message });
   }
@@ -59,6 +73,8 @@ const Login = async (req, res) => {
 // logout
 const Logout = async (req, res) => {
   try {
+    // Clear the JWT cookie by setting it with an empty value and maxAge to 0
+    res.cookie("jwt","",{maxAge:0})
     res.status(200).json({ msg: "user logout" });
   } catch (err) {
     res.status(500).json({ msg: "Server error", error: err.message });
---
+++ Backend/utils/getToken.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const generateTokenAndSetCookie = (userId,res) => {
+    const token = jwt.sign({ userId}, process.env.JWT_SECRET, {
+        expiresIn: '2d' // 2 days
+      
+ })
+ res.cookie("jwt", token, {
+    maxAge: 2 * 24 * 60 * 60 * 1000, //2 days
+    // helps prevent cross-site scripting (XSS) attacks
+    httpOnly: true, // prevent XSS attacks
+    //Cross-Site Request Forgery (CSRF) Protection: Setting sameSite: "strict" ensures that the cookie will only be sent in requests originating from the same origin as the domain
+    sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development"
+ })
+}
+
+module.exports = generateTokenAndSetCookie;
